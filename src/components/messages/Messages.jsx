cdafd7b4c6367ed6f9f746c6f0890b7e0a91e255
gagandeep-28 <2226010147@uudoon.in>
2024-09-21 23:36:18 +0530

    messages loader and skeleton added

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -3,10 +3,14 @@ import { DUMMY_MESSAGES } from "../../dummy_data/dummy";
 import Message from "./Message";
 import { useEffect } from "react";
 import useMessages from "../../hooks/useMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton.jsx";
 const Messages = () => {
   const selectedConversation = useSelector(
     (state) => state?.conversation?.selectedConversation
-  )
+  );
+  const messagesLoading = useSelector(
+    (state) => state?.messages?.messagesLoading
+  );
   const { getConversationMessages } = useMessages();
   useEffect(() => {
     const initial = async () => {
@@ -15,11 +19,26 @@ const Messages = () => {
     initial();
   }, [selectedConversation?.id]);
   const messages = useSelector((state) => state?.messages?.messages);
+  if (messagesLoading) {
+    return (
+      <div className="px-4 flex-1 overflow-auto">
+        <MessageSkeleton />
+        <MessageSkeleton />
+        <MessageSkeleton />
+      </div>
+    );
+  }
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {messages?.map((message) => (
-        <Message key={message.id} message={message} />
-      ))}
+      {messages?.length > 0 &&
+        messages?.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      {messages?.length === 0 && (
+        <p className="text-center">
+          Please send a message to start a conversation
+        </p>
+      )}
     </div>
   );
 };
--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -29,6 +29,7 @@ const useMessages = () => {
       }
     } catch (error) {
       dispatch(setMessagesError(error));
+      dispatch(setMessages([]));
     }
   };
 
